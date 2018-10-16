9ad2e4c72d630b09cbf5f4946d722698d53781d6
Ndeye-Ada <ndeyetouada@gmail.com>
2018-10-16 22:19:31 +0000

    initial commit

---
+++ app/src/main/java/com/example/ndeyetou/pushgithub/PushGithub.java
@@ -0,0 +1,13 @@
+package com.example.ndeyetou.pushgithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PushGithub extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_push_github);
+    }
+}
